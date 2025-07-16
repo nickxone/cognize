77665b81e898cfed51f10cea31a0ad9450c2a4c7
Matvii Ustich <matvejustic@gmail.com>
2025-07-16 10:20:31 +0100

    Initial Commit

---
+++ Cognize/CognizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CognizeApp.swift
+//  Cognize
+//
+//  Created by Matvii Ustich on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CognizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

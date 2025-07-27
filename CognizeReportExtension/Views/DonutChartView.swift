d864eb5e2fffb3f881a5a193b096725bc761cecb
Matvii Ustich <matvejustic@gmail.com>
2025-07-27 18:51:38 +0100

    Make DonutChartView interactive

--- CognizeReportExtension/Views/DonutChartView.swift
+++ CognizeReportExtension/Views/DonutChartView.swift
@@ -19,6 +19,10 @@ struct AppUsage: Identifiable {
 struct DonutChartView: View {
     let activity: TotalActivityView.Configuration
     
+    @State private var selectedEntry: TimeInterval?
+    @State private var selectedApp: AppUsage?
+    @State private var hideTimer: Timer?
+    
     var usageData: [AppUsage] {
         let sorted = activity.totalUsageByCategory
             .map { AppUsage(appName: $0.key.localizedDisplayName ?? "Unknown", usage: $0.value) }
@@ -36,26 +40,44 @@ struct DonutChartView: View {
     
     var body: some View {
         VStack {
-            Chart(usageData) { usage in
+            Chart(usageData) { appUsage in
                 SectorMark(
-                    angle: .value("Usage", usage.usage),
+                    angle: .value("Usage", appUsage.usage),
                     innerRadius: .ratio(0.618),
+                    outerRadius: selectedApp?.appName == appUsage.appName ? .ratio(1.0) : .ratio(0.85),
                     angularInset: 1.5
                 )
                 .cornerRadius(5)
-                .foregroundStyle(by: .value("App", usage.appName))
+                .foregroundStyle(by: .value("App", appUsage.appName))
+                .opacity(selectedApp == nil ? 1.0 : (appUsage.appName == selectedApp?.appName ? 1 : 0.3))
+            }
+            .chartAngleSelection(value: $selectedEntry)
+            .onChange(of: selectedEntry) { oldValue, newValue in
+                if let newValue {
+                    withAnimation {
+                        getSelectedApp(value: newValue)
+                    }
+                    restartHideTimer()
+                }
             }
             .chartLegend(.visible)
             .chartBackground { chartProxy in
                 GeometryReader { geometry in
                     let frame = geometry[chartProxy.plotFrame!]
                     VStack {
-                        Text("Most Used App")
-                            .font(.callout)
-                            .foregroundStyle(.secondary)
-                        Text(usageData[0].appName)
-                            .font(.title2.bold())
-                            .foregroundStyle(.primary)
+                        if let selectedApp {
+                            Text("\(selectedApp.appName)")
+                                .font(.callout)
+                                .foregroundStyle(.secondary)
+                            Text("\(formattedDuration(selectedApp.usage))")
+                        } else {
+                            Text("Most Used App")
+                                .font(.callout)
+                                .foregroundStyle(.secondary)
+                            Text(usageData[0].appName)
+                                .font(.title2.bold())
+                                .foregroundStyle(.primary)
+                        }
                     }
                     .position(x: frame.midX, y: frame.midY)
                 }
@@ -65,12 +87,33 @@ struct DonutChartView: View {
         .navigationTitle("Total Usage")
     }
     
-    func formattedDuration(_ duration: TimeInterval) -> String {
+    private func getSelectedApp(value: TimeInterval) {
+        var cumulativeTotal: TimeInterval = 0
+        _ = usageData.first { appUsage in
+            cumulativeTotal += appUsage.usage
+            if value <= cumulativeTotal {
+                selectedApp = appUsage
+                return true
+            }
+            return false
+        }
+    }
+    
+    private func formattedDuration(_ duration: TimeInterval) -> String {
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.hour, .minute]
         formatter.unitsStyle = .short
         formatter.zeroFormattingBehavior = .dropAll
         return formatter.string(from: duration) ?? "0m"
+    }
+    
+    private func restartHideTimer() {
+        hideTimer?.invalidate()
+        hideTimer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { _ in
+            withAnimation(.easeInOut(duration: 0.35)) {
+                selectedApp = nil
+            }
+        }
     }
 }
 
